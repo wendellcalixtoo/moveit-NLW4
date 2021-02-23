3eb408bbb453ca1f00514edf7b130d50a78a2411
wendellcalixtoo <wendellcalixtoo@gmail.com>
2021-02-22 22:34:46 -0300

    Style: Add style global

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-
+import { ExperienceBar } from './components/ExperienceBar'
 import './styles/global.css'
 
 function App() {
   return (
-    <div>
-
+    <div className="container">
+      <ExperienceBar />
     </div>
   );
 }
